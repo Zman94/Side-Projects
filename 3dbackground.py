8b47717d489642d7522d3758b3d88230ceddb7ec
Zman94 <zgleason94@gmail.com>
2016-12-12 16:58:31 -0600

    commit

--- 3dbackground.py
+++ 3dbackground.py
@@ -42,23 +42,6 @@ class terrain:
 
             pygame.display.flip()
 
-def rotatePoint(centerPoint,point,angle):
-    """Rotates a point around another centerPoint. Angle is in degrees.
-    Rotation is counter-clockwise"""
-    angle = math.radians(angle)
-    temp_point = point[0]-centerPoint[0] , point[1]-centerPoint[1]
-    temp_point = ( temp_point[0]*math.cos(angle)-temp_point[1]*math.sin(angle) , temp_point[0]*math.sin(angle)+temp_point[1]*math.cos(angle))
-    temp_point = temp_point[0]+centerPoint[0] , temp_point[1]+centerPoint[1]
-    return temp_point
-
-def rotatePolygon(polygon,theta):
-    """Rotates the given polygon which consists of corners represented as (x,y), around the ORIGIN, clock-wise, theta degrees"""
-    theta = math.radians(theta)
-    rotatedPolygon = []
-    for corner in polygon:
-        rotatedPolygon.append(( corner[0]*math.cos(theta)-corner[1]*math.sin(theta) , corner[0]*math.sin(theta)+corner[1]*math.cos(theta)) )
-    return rotatedPolygon
-
 def main():
     MainWindow = terrain()
     MainWindow.terrainLoop()
