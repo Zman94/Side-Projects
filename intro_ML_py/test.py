80eba9d2fe28c8c04c79ec2cf7f41434bde1345f
Zman94 <zgleason94@gmail.com>
2017-01-27 15:57:04 -0600

    Moved personal projects to separate folder. Added another personal project

--- intro_ML_py/test.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-data1 = [[1, 2, 3], [2, 3, 4], [5, 6, 7]]
-with open('data.pickle', 'wb') as f:
-    pickle.dump(data1, f)
---
+++ zproj/agario/empty-example/sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+	createCanvas(200,200);
+}
+
+function draw() {
+	ellipse(50, 50, 80, 80);
+}
