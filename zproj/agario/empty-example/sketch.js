7003874f65ada97c9716030c960fa8ee3c2d8834
Zman94 <zgleason94@gmail.com>
2017-01-28 02:56:00 -0600

    Working on agario

--- zproj/agario/empty-example/sketch.js
+++ zproj/agario/empty-example/sketch.js
@@ -1,7 +1,32 @@
+var blob;
+
+var blobs = [];
+
 function setup() {
-	createCanvas(200,200);
+	createCanvas(600,600);
+	blob = new Blob(0, 0, 64);
+	for (var i = 0; i < 50; i++){
+		var x = random(-width,width);
+		var y = random(-height,height);
+		blobs[i] = new Blob(x, y, 16);
+	}
 }
 
 function draw() {
-	ellipse(50, 50, 80, 80);
+	background(0);
+
+	translate(width/2, height/2);
+	scale(64/blob.r);
+	translate(-blob.pos.x, -blob.pos.y);
+
+	blob.show();
+	blob.update();
+
+	for (var i = blobs.length-1; i >= 0; i--){
+		if (blob.eats(blobs[i])){
+			blobs.splice(i,1)
+		}
+		blobs[i].show();
+	}
 }
+
