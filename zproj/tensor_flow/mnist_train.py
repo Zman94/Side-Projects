2cfff673c8b5f6e73d85ac08634457ee396a2e12
Zman94 <zgleason94@gmail.com>
2017-02-08 12:50:54 -0600

    Added tensor_flow tutorial under zproj

---
+++ zproj/tensor_flow/mnist_train.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+#test comments
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+x = tf.placeholder(tf.float32, [None, 784])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
